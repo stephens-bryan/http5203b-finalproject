7848c26ad1720bed9627362007134656ef360ec2
Bryan Stephens <bryanstephensdevelopment@gmail.com>
2017-04-29 18:43:30 -0400

    paired infoWindows with markers

--- script.js
+++ script.js
@@ -1,72 +1,295 @@
+var map, geocoder;
+function initMap()
+{
+    var center = new google.maps.LatLng(43.7181552,-79.5184852);
+    var mapOptions = {
+        zoom: 14,
+        center: center,
+        // styling from map came from Google's map style generator: https://mapstyle.withgoogle.com/
+        styles : [
+            {
+                "elementType": "geometry",
+                "stylers": [
+                    {
+                        "color": "#242f3e"
+                    }
+                ]
+            },
+            {
+                "elementType": "labels.text.fill",
+                "stylers": [
+                    {
+                        "color": "#746855"
+                    }
+                ]
+            },
+            {
+                "elementType": "labels.text.stroke",
+                "stylers": [
+                    {
+                        "color": "#242f3e"
+                    }
+                ]
+            },
+            {
+                "featureType": "administrative",
+                "elementType": "geometry",
+                "stylers": [
+                    {
+                        "visibility": "off"
+                    }
+                ]
+            },
+            {
+                "featureType": "administrative.locality",
+                "elementType": "labels.text.fill",
+                "stylers": [
+                    {
+                        "color": "#d59563"
+                    }
+                ]
+            },
+            {
+                "featureType": "poi",
+                "stylers": [
+                    {
+                        "visibility": "off"
+                    }
+                ]
+            },
+            {
+                "featureType": "poi",
+                "elementType": "labels.text.fill",
+                "stylers": [
+                    {
+                        "color": "#d59563"
+                    }
+                ]
+            },
+            {
+                "featureType": "poi.park",
+                "elementType": "geometry",
+                "stylers": [
+                    {
+                        "color": "#263c3f"
+                    }
+                ]
+            },
+            {
+                "featureType": "poi.park",
+                "elementType": "labels.text.fill",
+                "stylers": [
+                    {
+                        "color": "#6b9a76"
+                    }
+                ]
+            },
+            {
+                "featureType": "road",
+                "elementType": "geometry",
+                "stylers": [
+                    {
+                        "color": "#38414e"
+                    }
+                ]
+            },
+            {
+                "featureType": "road",
+                "elementType": "geometry.stroke",
+                "stylers": [
+                    {
+                        "color": "#212a37"
+                    }
+                ]
+            },
+            {
+                "featureType": "road",
+                "elementType": "labels.icon",
+                "stylers": [
+                    {
+                        "visibility": "off"
+                    }
+                ]
+            },
+            {
+                "featureType": "road",
+                "elementType": "labels.text.fill",
+                "stylers": [
+                    {
+                        "color": "#9ca5b3"
+                    }
+                ]
+            },
+            {
+                "featureType": "road.highway",
+                "elementType": "geometry",
+                "stylers": [
+                    {
+                        "color": "#746855"
+                    }
+                ]
+            },
+            {
+                "featureType": "road.highway",
+                "elementType": "geometry.stroke",
+                "stylers": [
+                    {
+                        "color": "#1f2835"
+                    }
+                ]
+            },
+            {
+                "featureType": "road.highway",
+                "elementType": "labels.text.fill",
+                "stylers": [
+                    {
+                        "color": "#f3d19c"
+                    }
+                ]
+            },
+            {
+                "featureType": "transit",
+                "stylers": [
+                    {
+                        "visibility": "off"
+                    }
+                ]
+            },
+            {
+                "featureType": "transit",
+                "elementType": "geometry",
+                "stylers": [
+                    {
+                        "color": "#2f3948"
+                    }
+                ]
+            },
+            {
+                "featureType": "transit.station",
+                "elementType": "labels.text.fill",
+                "stylers": [
+                    {
+                        "color": "#d59563"
+                    }
+                ]
+            },
+            {
+                "featureType": "water",
+                "elementType": "geometry",
+                "stylers": [
+                    {
+                        "color": "#17263c"
+                    }
+                ]
+            },
+            {
+                "featureType": "water",
+                "elementType": "geometry.stroke",
+                "stylers": [
+                    {
+                        "visibility": "simplified"
+                    }
+                ]
+            },
+            {
+                "featureType": "water",
+                "elementType": "labels.text.fill",
+                "stylers": [
+                    {
+                        "color": "#515c6d"
+                    }
+                ]
+            },
+            {
+                "featureType": "water",
+                "elementType": "labels.text.stroke",
+                "stylers": [
+                    {
+                        "color": "#17263c"
+                    }
+                ]
+            }
+        ]
+    };
+    map = new google.maps.Map(document.querySelector("#map"), mapOptions);
+} // end of initMap()
+
+var form = document.querySelector("form");
 var formreturn = {
     'address' : this.address
 };
-
-// note(bryanstephens): since data is not kept following a form submission, my workaround is to create a local session item to store the value of the address
-var form = document.querySelector("form");
 form.addEventListener("submit", function(event){
     formreturn.address = form.elements.address.value;
-    localStorage.setItem("address", formreturn.address);
-});
-
-function initMap()
-{
-    var map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 14
-    });
-    var gcoder = new google.maps.Geocoder();
-
-    // note(bryanstephens): since a user is not forced to submit a value in the form field, provide a default address of the school
-    var address;
-    if (localStorage.getItem("address") === null)
-    {
-        address = "205 Humber College Blvd";
-    }
-    else
-    {
-        address = localStorage.getItem("address");
-    }
-
-
-    gcoder.geocode(
-        { 'address': address},
-        function (results, status){
-            if (status == 'OK'){
-                var loc = [];
-                //address geocoded successfully
-                map.setCenter(results[0].geometry.location);
-                var marker = new google.maps.Marker({
-                    position: results[0].geometry.location,
-                    map: map
-                });
-                loc[0] = results[0].geometry.location.lat();
-                loc[1] = results[0].geometry.location.lng();
-                // how to grab lng & lat values from user input
-                console.log(loc[1] + " " + loc[0]);
-                localStorage.removeItem("address");
-            } // end of statuscode check (AJAX)
-        } // end of AJAX call
-    );
-} // end of initMap()
-
-jQuery.ajax({
-    url: 'https://lcboapi.com/stores?lat=43.8612895&lon=-79.3178956&products?q=beer',
-    headers:
+    var address = formreturn.address;
+    geocoder = new google.maps.Geocoder();
+    geocoder.geocode(
+        {'address': address},
+        function(results, status)
         {
-            Authorization: 'Token MDphOTlkZjgwMC0yYzgyLTExZTctOTQyNi1lZmI3ZDc5NmUzZWY6ZHdYZlJVNlp5SHZiWXFBMUdFdEpBa1VyZjBYSm40TmpHa2tl'
-        }
-}).then(function(data) {
-    var results = data["result"];
-    //console.log(results);
-    //console.log(data);
-    // for(var prop in data){
-    //     if (data.hasOwnProperty(prop)){
-    //         console.log(prop + " " + data[prop]);
-    //     }
-    // }
-    for (var i = 0; i < results.length; i++){
-        console.log(results[i]["name"]);
-    }
+            if (status === "OK")
+            {
+                map.setCenter(results[0].geometry.location);
+                var lat = results[0].geometry.location.lat();
+                var lng = results[0].geometry.location.lng();
+                jQuery.ajax({
+                    url: 'https://lcboapi.com/stores?lat=' + lat + '&lon=' + lng +'&products?q=beer',
+                    headers:
+                        {
+                            Authorization: 'Token MDphOTlkZjgwMC0yYzgyLTExZTctOTQyNi1lZmI3ZDc5NmUzZWY6ZHdYZlJVNlp5SHZiWXFBMUdFdEpBa1VyZjBYSm40TmpHa2tl'
+                        }
+                }).then(function(data) {
+                    var results = data["result"];
+                    for (var i = 0; i < results.length; i++){
+
+                        // Note(bryanstephens): positions sets the lng & lat of each returned result
+                        var positions = [
+                            {lat: results[i]["latitude"], lng: results[i]["longitude"] }
+                        ];
+
+
+                        var contentString = [
+
+                        ];
+
+
+
+                        //console.log(infoWindow);
+                        var markers = positions.map(function(location, k){
+                            var marker = new google.maps.Marker({
+                                position: location,
+                                map: map
+                            });
+                            var infoWindow = new google.maps.InfoWindow({
+                                content: '<div>' +
+                                '<h2>' + results[i]["name"]  + '</h2>' +
+                                '<p> Distance away(from entered Addressed): ' + results[i]["distance_in_meters"] + '</p>' +
+                                '</div>'
+                            });
+                            google.maps.event.addListener(marker,'click', function(){
+                                infoWindow.open(map,marker);
+                            })
+                        });
+
+                }// end of for loop
+                });
+            }// check if status is not OK
+            else
+            {
+                console.log(status);
+            }
+        });// end of google geocode event
+    event.preventDefault();
 });
 
-// var results = data["result"][0]["address_line_1"]
+// google.maps.event.addListener('click', function(){
+//console.log(location);
+// });
+/*
+* {
+ content: contentString[i][i]
+
+
+ ****8 {
+
+ }
+ }*/
